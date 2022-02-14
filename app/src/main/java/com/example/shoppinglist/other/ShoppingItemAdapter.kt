baa92491e0bb7600c39d6f584728eb1b618e1763
chayan-dev <chayandas0308@gmail.com>
2022-02-14 23:48:03 +0530

    recyclerview fixed

--- app/src/main/java/com/example/shoppinglist/other/ShoppingItemAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/other/ShoppingItemAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.db.entities.ShoppingItem
+import com.example.shoppinglist.databinding.ShopppingItemBinding
 import com.example.shoppinglist.ui.shoppinglist.ShoppingViewModel
 
 class ShoppingItemAdapter(
@@ -15,37 +16,38 @@ class ShoppingItemAdapter(
     private val viewModel:ShoppingViewModel
 ): RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
 
-    lateinit var tvName:TextView
-    lateinit var ivdelete:ImageView
+//    lateinit var tvName:TextView
+//    lateinit var ivdelete:ImageView
 
-    class ShoppingViewHolder(val binding: shopping ): RecyclerView.ViewHolder()
+    inner class ShoppingViewHolder(val itemBinding: ShopppingItemBinding ): RecyclerView.ViewHolder(itemBinding.root)
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
 
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.shoppping_item,parent,false)
+        val view=ShopppingItemBinding.inflate(LayoutInflater.from(parent.context))
         return ShoppingViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
 
-        tvName=holder.itemView.findViewById(R.id.tvName)
-        ivdelete=holder.itemView.findViewById<ImageView>(R.id.ivDelete)
+//        tvName=itemBinding.tvName
+//        ivdelete=holder.itemView.findViewById<ImageView>(R.id.ivDelete)
 
 
         val curShoppingItem=items[position]
 
-        holder.itemView.tvName.text="${curShoppingItem.amount}"
+        holder.itemBinding.tvName.text=curShoppingItem.name
+        holder.itemBinding.tvAmount.text="${curShoppingItem.amount}"
 
-        holder.itemView.ivdelete.setOnClickListener{
+        holder.itemBinding.ivDelete.setOnClickListener{
             viewModel.delete(curShoppingItem)
         }
-        holder.itemView.ivPLus.setOnClickListener{
+        holder.itemBinding.ivPlus.setOnClickListener{
             curShoppingItem.amount++
             viewModel.upsert(curShoppingItem)
         }
-        holder.itemView.ivMinus.SetOnClickListener{
-            if(curShopping.amount>0){
+        holder.itemBinding.ivMinus.setOnClickListener{
+            if(curShoppingItem.amount>0){
                 curShoppingItem.amount--
                 viewModel.upsert(curShoppingItem)
             }
--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,21 +2,56 @@ package com.example.shoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.db.ShoppingDatabase
+import com.example.shoppinglist.data.db.entities.ShoppingItem
 import com.example.shoppinglist.data.repositories.ShoppingRepository
+import com.example.shoppinglist.databinding.ActivityShoppingBinding
+import com.example.shoppinglist.databinding.ShopppingItemBinding
+import com.example.shoppinglist.other.ShoppingItemAdapter
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(),KodeinAware {
+
+    override val kodein by kodein()
+    private val factory:ShoppingViewModelFactory by instance()
+
+
+    lateinit var binding:ActivityShoppingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
-
-        val database=ShoppingDatabase(this)
-        val repository=ShoppingRepository(database)
-        val factory=ShoppingViewModelFactory(repository)
+        binding= ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val viewModel=ViewModelProviders.of(this,factory).get(ShoppingViewModel::class.java)
+
+        val adapter=ShoppingItemAdapter(listOf(),viewModel)
+
+        binding.rvShoppingItems.layoutManager=LinearLayoutManager(this)
+        binding.rvShoppingItems.adapter=adapter
+
+        viewModel.getAllShoppingItem().observe(this, Observer {
+            adapter.items=it
+            adapter.notifyDataSetChanged()
+
+        })
+
+        binding.fab.setOnClickListener{
+            AddShoppingItemDialog(this,
+            object :AddDialogListener{
+                override fun onAddButtonClicked(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+            }).show()
+        }
+
 
     }
 }
